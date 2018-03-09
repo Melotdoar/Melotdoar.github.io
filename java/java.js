39511c465d5d04faadfe24d25bd7043f67591533
Melotdoar <37212276+Melotdoar@users.noreply.github.com>
2018-03-09 12:11:31 +0100

    woo
    sås

---
+++ java/java.js
@@ -0,0 +1,8 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script>
+$(document).ready(function () {
+    $("#flyttbar").draggable () ;
+
+});
+</script>
