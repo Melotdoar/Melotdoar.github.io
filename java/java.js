ed691f7fa0a14924f2c152ee16620d3b77b24ef8
oscarwernqvist <32632202+oscarwernqvist@users.noreply.github.com>
2018-03-11 12:58:30 +0100

    Bra progress
    går bra nu

--- java/java.js
+++ java/java.js
@@ -1,17 +1,45 @@
 
 $(document).ready(() => {
     
-    $('#menudrop').hide()
+
+    var latestmain = "#sida1main"
+
+    $('#menudrop, #close, #loginmain, #welcomemain').hide()
 
     $('#searchicon').on('click', () => {
-        $('#rubrik, #menu').hide();
+        $('#rubrik, #menu, #searchicon').hide();
         $('#searchbar').css("display", "block");
+        $('#close').show();
     });
+
+    
+    $('#close').on('click', () => {
+        $('#rubrik, #menu, #searchicon').show();
+        $('#searchbar').css("display", "none");
+        $('#close').hide()
+    });
+
     $('#menu').on('click', () => {
         $('#menudrop').css('left', '0');
         $('#menudrop').slideToggle(400);
     });
 
+    $('#loginbutton').on('click', () => {
+        $(latestmain).hide();
+        $('#menudrop').slideToggle(150);
+        $('#loginmain').show();
+        latestmain = "#loginmain";
+    });
+
+    $('#loginconfirm').on('click', () => {
+        $(latestmain).hide();
+        $('#welcomemain').show();
+        latestmain = "#welcomemain";
+    });
+
+    function pr() {
+        document.getElementById("result").innerHTML = document.getElementById('username').value;
+      }
 
 });
 
